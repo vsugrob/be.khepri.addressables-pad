95b476da9880eb887eaff9bc5a13a6af0ea1cf03
vsugrob <vsugrob@hotmail.com>
2020-10-07 14:33:41 +0300

    Add means to distinguish aab and apk builds in runtime.

--- Editor/PlayAssetPackBundlesPreprocessor.cs
+++ Editor/PlayAssetPackBundlesPreprocessor.cs
@@ -1,24 +1,42 @@
-﻿using Khepri.PlayAssetDelivery.Editor;
+﻿using Khepri.AssetDelivery;
+using Khepri.PlayAssetDelivery.Editor;
+using System;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UDebug = UnityEngine.Debug;
 
-public class PlayAssetPackBundlesPreprocessor : IPreprocessBuildWithReport
-{
+public class PlayAssetPackBundlesPreprocessor : IPreprocessBuildWithReport {
     public int callbackOrder => 2;
 
-    public void OnPreprocessBuild(BuildReport report)
-    {
-        if (report.summary.platform != BuildTarget.Android)
-        {
+    public void OnPreprocessBuild ( BuildReport report ) {
+        var isAabBuild = report.summary.outputPath.EndsWith ( ".aab", StringComparison.InvariantCultureIgnoreCase );
+        var buildCfg = GetOrCreateConfig ();
+        buildCfg.IsAabBuild = isAabBuild;
+        EditorUtility.SetDirty ( buildCfg );
+		AssetDatabase.SaveAssets ();
+		AssetDatabase.Refresh ();
+        if ( !isAabBuild || report.summary.platform != BuildTarget.Android )
             return;
-        }
-        UDebug.Log($"[{nameof(PlayAssetPackBundlesPreprocessor)}.{nameof(OnPreprocessBuild)}]");
+
+        UDebug.Log (
+            $"[{nameof ( PlayAssetPackBundlesPreprocessor )}.{nameof ( OnPreprocessBuild )}]" +
+            " This is aab-build. Remove addressable asset bundles from StreamingAssets folder."
+        );
         foreach (var bundle in AssetPackBuilder.GetBundles(Addressables.PlayerBuildDataPath))
         {
             bundle.DeleteFile();
         };
     }
+
+    private static AndroidBuildTypeConfig GetOrCreateConfig () {
+		var config = AssetDatabase.LoadAssetAtPath <AndroidBuildTypeConfig> ( AndroidBuildTypeConfig.Path );
+		if ( config == null ) {
+			config = ScriptableObject.CreateInstance <AndroidBuildTypeConfig> ();
+			AssetDatabase.CreateAsset ( config, AndroidBuildTypeConfig.Path );
+		}
+		return config;
+	}
 }
---
+++ Runtime/AndroidBuildTypeConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Khepri.AssetDelivery {
+    public class AndroidBuildTypeConfig : ScriptableObject {
+        public const string FileName = "AndroidBuildType";
+        public const string Path = "Assets/Resources/" + FileName + ".asset";
+        public bool IsAabBuild;
+        private static AndroidBuildTypeConfig _singleton;
+        public static AndroidBuildTypeConfig Singleton {
+            get {
+                if ( _singleton == null ) {
+                    _singleton = Resources.Load <AndroidBuildTypeConfig> ( FileName );
+                    if ( _singleton == null )
+                        _singleton = CreateInstance <AndroidBuildTypeConfig> ();
+                }
+
+                return _singleton;
+            }
+        }
+    }
+}
